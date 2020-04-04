af7a4f38b4bf1257a73526ba2ca8001445410923
Alonsogcl <62973685+Alonsogcl@users.noreply.github.com>
2020-04-03 23:41:06 -0600

    Confirmación de usuario

--- typescript/clases.ts
+++ typescript/clases.ts
@@ -1,5 +1,5 @@
 (() => {
-  //Para comprobar el usuario
+  //Comprobación de usuario nuevamente
   //La primera letra de la clase siempre tiene que ir en mayuscula
   // class Avenger {
   /*Para no tener que inicializar se crea un constructor
